febeb11f152ba9696d671f20c4b27797acdcd941
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-17 15:28:03 -0500

    added engineer class to engineer.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,19 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// This will pull in the Employee class data and allow us to extend it below
+var Employee = require('./Employee.js')
+// This Engineer class will extend the existing Employee class to it, and then add the Engineer specific data
+class Engineer extends Employee {
+    constructor(id, name, email, github) {
+        super(id, name, email) // pulling this data from Employee class
+        this.github = github
+    }
+    //We have to include these methods per the assignment instructions so that the technology will match back to the HTML and pass all of the tests provided.
+    getGithub() {
+        return this.github
+    }
+    getRole() {
+        return "Engineer"
+    }
+
+}
+// Allowing the Manager data to be exported
+module.exports = Engineer
