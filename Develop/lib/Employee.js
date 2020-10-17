eb4d8547e2382dfa3d84f1110dd60c21ffc37c9c
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-17 15:26:15 -0500

    added Manager class to manager.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,10 +1,11 @@
-// TODO: Write code to define and export the Employee class
+// This Employee class has the general info all employees will have and creates a constructor to house all of this data. This class will then be extended to all the employee types.
 class Employee {
     constructor(id, name, email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
+    //We have to include these methods per the assignment instructions so that the technology will match back to the HTML and pass all of the tests provided.
     getId() {
         return this.id
     }
@@ -14,6 +15,9 @@ class Employee {
     getEmail() {
         return this.email
     }
+    getRole() {
+        return Employee
+    }
 }
-
+// Allowing the Employee data to be exported
 module.exports = Employee
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,19 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// This will pull in the Employee class data and allow us to extend it below
+var Employee = require('./Employee.js')
+// This Manager class will extend the existing Employee class to it, and then add the Manager specific data
+class Manager extends Employee {
+    constructor(id, name, email, officeNumber) {
+        super(id, name, email) // pulling this data from Employee class
+        this.officeNumber = officeNumber
+    }
+    //We have to include these methods per the assignment instructions so that the technology will match back to the HTML and pass all of the tests provided.
+    getOfficeNumber() {
+        return this.officeNumber
+    }
+    getRole() {
+        return "Manager"
+    }
+
+}
+// Allowing the Manager data to be exported
+module.exports = Manager
